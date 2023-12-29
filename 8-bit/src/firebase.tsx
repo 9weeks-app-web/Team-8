d6534c050da657f8a85b19c42f5f5e2573fb0255
an seungji <ansj8777@gmail.com>
2023-12-29 16:49:39 +0900

    ✨[Feature]환경변수 추가

--- 8-bit/src/firebase.tsx
+++ 8-bit/src/firebase.tsx
@@ -1,19 +1,16 @@
-
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCMYfD0xOVeqjlqclTKCQCXnh01V7-ol98",
-  authDomain: "bit-test-1bf51.firebaseapp.com",
-  databaseURL: "https://bit-test-1bf51-default-rtdb.firebaseio.com",
-  projectId: "bit-test-1bf51",
-  storageBucket: "bit-test-1bf51.appspot.com",
-  messagingSenderId: "798843212797",
-  appId: "1:798843212797:web:963708f6b526e59fc77f94",
-  measurementId: "G-BTG85X40XG"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 
 export default app;
-
-
