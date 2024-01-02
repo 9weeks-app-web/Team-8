2c076ff93c52d6e7f7ad4d8b6f92f1652061e6b4
an seungji <ansj8777@gmail.com>
2024-01-02 18:35:12 +0900

    💄[Design]공통 스타일 설정 #9

--- 8-bit/src/styles/common.ts
+++ 8-bit/src/styles/common.ts
@@ -1,4 +1,77 @@
 export const Common = {
+  colors: {
+    neutral: {
+      100: "#030303",
+      90: "#1A1A1A",
+      80: "#333333",
+      70: "#4D4D4D",
+      60: "#666666",
+      50: "#808080",
+      40: "#999999",
+      30: "#B3B3B3",
+      20: "#CCCCCC",
+      10: "#E6E6E6",
+      5: "#F3F3F3",
+      0: "#CED4DA",
+    },
+    primary: {
+      150: "#0043C0",
+      100: "#0059FF",
+      90: "#196AFF",
+      80: "#337AFF",
+      70: "#4C8BFF",
+      60: "#669BFF",
+      50: "#7FACFF",
+      40: "#99BDFF",
+      30: "#B2CDFF",
+      20: "#CCDEFF",
+      10: "#E5EEFF",
+      5: "#F5F8FF",
+      0: "#CED4DA",
+    },
+    stroke: {
+      blue: "#99BDFF",
+      10: "#E6E6E6",
+      5: "#F3F3F3",
+    },
+    background: {
+      100: "#666666",
+      50: "#A9A9A9",
+      5: "#F8F8F9",
+      blue: "#F5F8FF",
+    },
+    system: {
+      warning: "#FF0000",
+      success: "#07A320",
+    },
+  },
+  font: {
+    size: {
+      header: "34px",
+      xxxl: "24px",
+      xxl: "22px",
+      xl: "20px",
+      lg: "18px",
+      md: "16px",
+      sm: "14px",
+      xs: "12px",
+      xxs: "10px",
+    },
+    weight: {
+      bold: "700",
+      semibold: "600",
+      medium: "500",
+      regular: "400",
+    },
+  },
+  space: {
+    xs: "8px",
+    s: "16px",
+    md: "24px",
+    lg: "32px",
+    xl: "40px",
+    xxl: "56px",
+  },
   a11yHidden: `
   overflow: hidden;
   position: absolute !important;
@@ -129,7 +202,7 @@ export const Common = {
     }
     table {
       border-collapse: collapse;
-      border-spacing: 0;
+      border-: 0;
     }
     button {
       cursor: pointer;
@@ -142,7 +215,7 @@ export const Common = {
       -webkit-font-smoothing: antialiased;
     }
     html {
-      font-size: 18px;
+      font-size: 16 px;
       font-family:
         "Pretendard Variable",
         Pretendard,
