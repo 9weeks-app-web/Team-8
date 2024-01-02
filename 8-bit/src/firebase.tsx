c286dcad27638d441b28a8261d6d251b3a2ab17d
jojunsoo <junsoo850@naver.com>
2024-01-02 23:45:22 +0900

    💄[Design]로그인페이지 설정(1차)

--- 8-bit/src/firebase.tsx
+++ 8-bit/src/firebase.tsx
@@ -1,16 +1,17 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: import.meta.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: import.meta.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: import.meta.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: import.meta.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_APP_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 
 export default app;
+
--- 8-bit/src/pages/SignIn.tsx
+++ 8-bit/src/pages/SignIn.tsx
@@ -1,77 +1,157 @@
-import styled from "@emotion/styled";
-import { getAuth, signInWithEmailAndPassword} from "firebase/auth";
-import { useState } from "react";
+  import styled from "@emotion/styled";
+  import { getAuth, signInWithEmailAndPassword} from "firebase/auth";
+  import { useState } from "react";
+  import { useNavigate } from "react-router-dom";
 
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  background-color: #f0f2f5;
+  const Container = styled.div`
+    display : flex;
+    flex-direction : column;
+    align-items : center;
+  `;
+
+  const Logo = styled.div`
+    display : flex;
+    flex-direction : column;
+    align-items : center;
+    margin-top : 2%;
+
+    img {
+      width : 187px;
+      height : 49px;
+      margin-bottom : 20%;
+    }
+  `;
+
+  const LoginForm = styled.form`
+    padding : 20px;
+    background : white;
+    width : 400px;
+  `;
+
+  const Input = styled.input`
+    display : block;
+    width : 100%;
+    padding : 10px;
+    margin-bottom : 10px;
+    border-radius : 4px;
+    border : 1px solid #ccc;
+  `;
+
+  const SaveId = styled.label`
+    display : flex;
+    align-items : center;
+    margin-top : 5%;
+    margin-bottom : 5%;
+  `;
+
+  const SerchIdPw = styled.div`
+    margin-left : 18%;
+
+    a {
+      text-decoration : none;
+      color : black;
+    }
+  `;
+
+  const ButtonContainer = styled.div`
+    display : flex;
+    justify-content : center;
+  `;
+
+  const Button = styled.button`
+    width: 217px;
+    height: 56px;
+    border: none;
+    background-color: '#337AFF';
+    color: white;
+    border-radius: 10px;
+    cursor: 'pointer';
+    margin: auto;
+    transition: background-color 0.3s;
+
+    &:hover {
+      background-color: '#0059FF';
+    }
 `;
 
-const LoginForm = styled.form`
-  padding: 20px;
-  background: white;
-  border-radius: 8px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-`;
+  const SnsContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    gap : 25px;
+    
+    img {
+      witdh : 64px;
+      height : 64px;
+    }
+  `;
 
-const Input = styled.input`
-  display: block;
-  width: 100%;
-  padding: 10px;
-  margin-bottom: 10px;
-  border-radius: 4px;
-  border: 1px solid #ccc;
-`;
+  function SignIn() {
 
-const Button = styled.button`
-  width: 100%;
-  padding: 10px;
-  border: none;
-  background-color: #007bff;
-  color: white;
-  border-radius: 4px;
-  cursor: pointer;
+    const navigate = useNavigate();
 
-  &:hover {
-    background-color: #0056b3;
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    
+    const signin = async (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      const auth = getAuth();
+
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("/");
+    }
+
+    return (
+      <Container>
+        <Logo>
+          <img src="/logo.svg" alt="로고이미지" />
+          <h3>로그인</h3>
+        </Logo>
+        <LoginForm onSubmit={signin}>
+          <p>이메일 입력</p>
+          <Input 
+            type="email" 
+            value={email} 
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="이메일을 입력하세요" 
+          />
+          <p>비밀번호 입력</p>
+          <Input 
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="비밀번호를 입력하세요" 
+          />
+
+          <SaveId>
+            <input type="checkbox"/>
+            <span>아이디 저장</span>
+            <SerchIdPw>
+            <span><a href="#">아이디찾기</a></span>
+            <span> | </span>
+            <span><a href="#">비밀번호 찾기</a></span>
+            </SerchIdPw>
+          </SaveId>
+
+          <ButtonContainer>
+            <Button type="submit">로그인</Button>
+          </ButtonContainer>
+        </LoginForm>
+
+        <p>SNS 계정으로 로그인</p>
+
+        <SnsContainer>
+          <img src="/kakaosns.svg" alt="카카오로그인" />
+          <img src="/naversns.svg" alt="네이버로그인" />
+          <img src="/googlesns.svg" alt="구글로그인" />
+          <img src="/applesns.svg" alt="애플로그인" />
+        </SnsContainer>
+
+        <span>회원가입하고 모든 레퍼런스를 모아보세요. <a href="/signup">회원가입</a></span>
+
+        <img src="/loginfooterimg.svg" alt="로그인하단이미지" />
+
+      </Container>
+    );
   }
-`;
 
-function SignIn() {
-
-  const [email, setEmail] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
-  
-  const signin = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const auth = getAuth();
-
-    const result = await signInWithEmailAndPassword(auth, email, password);
-    console.log("로그인성공 : " +  result);
-  }
-
-  return (
-    <Container>
-      <LoginForm onSubmit={signin}>
-        <Input 
-          type="email" 
-          value={email} 
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Eamil" 
-        />
-        <Input 
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Password" 
-        />
-        <Button type="submit">Login</Button>
-      </LoginForm>
-    </Container>
-  );
-}
-
-export default SignIn;
+  export default SignIn;
