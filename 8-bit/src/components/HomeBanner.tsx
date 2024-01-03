10f0534e78cca19c9a46ad4f7d267360d18b4328
an seungji <ansj8777@gmail.com>
2024-01-03 15:10:41 +0900

    ✨[Feature] 홈페이지 메인 배너 마크업, 스타일링 마무리 #15

--- 8-bit/src/components/HomeBanner.tsx
+++ 8-bit/src/components/HomeBanner.tsx
@@ -80,7 +80,7 @@ const SwiperWrapper = styled.div`
   }
 
   .mySwiper {
-    background-color: ${Common.colors.neutral[30]};
+    /* background-color: ${Common.colors.neutral[30]}; */
     width: 600px;
     height: 340px;
     display: flex;
@@ -142,11 +142,25 @@ function HomeBanner() {
           modules={[Autoplay, Pagination, Navigation]}
           className="mySwiper"
         >
-          <SwiperSlide>Slide 1</SwiperSlide>
-          <SwiperSlide>Slide 2</SwiperSlide>
-          <SwiperSlide>Slide 3</SwiperSlide>
-          <SwiperSlide>Slide 4</SwiperSlide>
-          <SwiperSlide>Slide 5</SwiperSlide>
+          <SwiperSlide>
+            <img src="https://picsum.photos/600/340" alt="스와이퍼 이미지" />
+          </SwiperSlide>
+          <SwiperSlide>
+            {" "}
+            <img src="https://picsum.photos/600/340" alt="스와이퍼 이미지" />
+          </SwiperSlide>
+          <SwiperSlide>
+            {" "}
+            <img src="https://picsum.photos/600/340" alt="스와이퍼 이미지" />
+          </SwiperSlide>
+          <SwiperSlide>
+            {" "}
+            <img src="https://picsum.photos/600/340" alt="스와이퍼 이미지" />
+          </SwiperSlide>
+          <SwiperSlide>
+            {" "}
+            <img src="https://picsum.photos/600/340" alt="스와이퍼 이미지" />
+          </SwiperSlide>
         </Swiper>
       </SwiperWrapper>
     </Section>
