3571d844d693c111bee36322101ed7d30c380750
an seungji <ansj8777@gmail.com>
2024-01-03 15:10:34 +0900

    ✨[Feature] 홈페이지 메인 배너 마크업, 스타일링 마무리 #15

--- 8-bit/src/components/HomeBanner.tsx
+++ 8-bit/src/components/HomeBanner.tsx
@@ -9,17 +9,22 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 import { Autoplay, Pagination, Navigation } from "swiper/modules";
+import Button from "@mui/material/Button";
 
 const Section = styled.section`
   width: 1440px;
   margin: 0 auto;
   display: flex;
-  justify-content: space-between;
+  /* gap: ${Common.space.lg}; */
+  justify-content: space-around;
   padding: ${Common.space.xxl};
-  background: ${Common.colors.background.blue};
+  background-color: ${Common.colors.background.blue};
 `;
 
 const SearchWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
   width: 600px;
   padding: 18px;
 `;
@@ -27,19 +32,65 @@ const SearchWrapper = styled.div`
 const SearchBar = styled.div`
   display: flex;
   flex-direction: column;
+  gap: ${Common.space.s};
+  & > span {
+    font-size: ${Common.font.size.xxl};
+    font-weight: ${Common.font.weight.semibold};
+  }
+`;
+
+const SearchInput = styled(TextField)`
+  & .MuiInputBase-root {
+    border-radius: 18px;
+    background-color: ${Common.colors.background[5]};
+  }
+`;
+
+const KeywordWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: ${Common.space.s};
+  width: 430px;
+  & > p {
+    font-size: ${Common.font.size.lg};
+    font-weight: ${Common.font.weight.semibold};
+    color: ${Common.colors.neutral[60]};
+  }
+`;
+
+const Keywords = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: ${Common.space.xs};
+  & > Button {
+    border: 1px solid ${Common.colors.primary[50]};
+    border-radius: 12px;
+    background-color: ${Common.colors.primary[10]};
+    color: ${Common.colors.neutral[100]};
+  }
 `;
 
 const SwiperWrapper = styled.div`
   .swiper-button-next,
   .swiper-button-prev {
     color: ${Common.colors.primary[80]};
+    ::after {
+      font-size: 30px;
+    }
   }
 
   .mySwiper {
-    background: ${Common.colors.neutral[30]}};
+    background-color: ${Common.colors.neutral[30]};
     width: 600px;
     height: 340px;
     display: flex;
+    border-radius: 10px;
+  }
+
+  .swiper-slide {
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 `;
 
@@ -49,7 +100,7 @@ function HomeBanner() {
       <SearchWrapper>
         <SearchBar>
           <span>레퍼런스 탐색의 모든 것, 스팩폴리오</span>
-          <TextField
+          <SearchInput
             variant="outlined"
             InputProps={{
               startAdornment: (
@@ -60,23 +111,26 @@ function HomeBanner() {
             }}
             placeholder="원하는 레퍼런스를 탐색해 보세요."
             fullWidth
-          />{" "}
+          />
         </SearchBar>
-        <div>
-          인기 키워드
-          <button>크리스마스</button>
-          <button>새해</button>
-          <button>겨울</button>
-          <button>레이아웃</button>
-          <button>웹디자인</button>
-          <button>렌더링 이미지</button>
-          <button>UX/UI</button>
-        </div>
+        <KeywordWrapper>
+          <p>인기 키워드</p>
+          <Keywords>
+            <Button>크리스마스</Button>
+            <Button>새해</Button>
+            <Button>겨울</Button>
+            <Button>레이아웃</Button>
+            <Button>웹디자인</Button>
+            <Button>렌더링 이미지</Button>
+            <Button>UX/UI</Button>
+          </Keywords>
+        </KeywordWrapper>
       </SearchWrapper>
       <SwiperWrapper>
         <Swiper
           spaceBetween={30}
           centeredSlides={true}
+          loop={true}
           autoplay={{
             delay: 2500,
             disableOnInteraction: false,
